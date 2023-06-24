41cd15b1fad8f1534a1d424a3361c78c7e883114
mrityunjay <kmrityunjay360@gmail.com>
2023-06-24 14:59:22 +0530

    first commit

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -19,14 +19,14 @@ const Home = () => {
             key,
             amount: order.amount,
             currency: "INR",
-            name: "6 Pack Programmer",
-            description: "Tutorial of RazorPay",
-            image: "https://avatars.githubusercontent.com/u/25058652?v=4",
+            name: "Mrityunjay Kumar",
+            description: "Payment by RazorPay",
+            image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.FtnqaDQ2FRF8ys8QRpFFrAHaHa%26pid%3DApi&f=1&ipt=5e0922a9f8d3659fc09983dda391d4a92f2dcc95a971ab2e3a229fa514fba94c&ipo=images",
             order_id: order.id,
             callback_url: "http://localhost:4000/api/paymentverification",
             prefill: {
-                name: "Gaurav Kumar",
-                email: "gaurav.kumar@example.com",
+                name: "Manish Kumar",
+                email: "ms432@example.com",
                 contact: "9999999999"
             },
             notes: {
@@ -45,8 +45,8 @@ const Home = () => {
 
             <Stack h={"100vh"} alignItems="center" justifyContent="center" direction={["column", "row"]}>
 
-                <Card amount={5000} img={"https://cdn.shopify.com/s/files/1/1684/4603/products/MacBookPro13_Mid2012_NonRetina_Silver.png"} checkoutHandler={checkoutHandler} />
-                <Card amount={3000} img={"http://i1.adis.ws/i/canon/eos-r5_front_rf24-105mmf4lisusm_32c26ad194234d42b3cd9e582a21c99b"} checkoutHandler={checkoutHandler} />
+                <Card amount={50000} img={"https://cdn.shopify.com/s/files/1/1684/4603/products/MacBookPro13_Mid2012_NonRetina_Silver.png"} checkoutHandler={checkoutHandler} />
+                <Card amount={30000} img={"http://i1.adis.ws/i/canon/eos-r5_front_rf24-105mmf4lisusm_32c26ad194234d42b3cd9e582a21c99b"} checkoutHandler={checkoutHandler} />
 
             </Stack>
         </Box>
